81caf9998d5dc1c1cc41f917f10956b861e1a572
aninath <aninath@cisco.com>
2019-08-27 10:05:21 +0530

    Added constants

---
+++ src/main/java/k8/PlaygroundConstants.java
@@ -0,0 +1,19 @@
+package k8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlaygroundConstants {
+
+	
+public static Map<String, String> imageMap = new HashMap();
+	
+	static{
+		imageMap.put("python3", "0cloud0/playground:python3");
+		imageMap.put("mariadb10", "0cloud0/playground:mysql");
+		imageMap.put("rhel7", "0cloud0/playground:rhelubi7");
+	}
+	
+	
+	
+}
